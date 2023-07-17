a9423ab78f869ffd8c8181232365b65326cd4623
Ryohei <harrry.kuma1225@gmail.com>
2023-07-17 13:28:46 +0900

    add

--- frontend/src/components/parts/Button/addbtn.tsx
+++ frontend/src/components/parts/Button/addbtn.tsx
@@ -1,30 +1,32 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+import {useState} from "react";
+import Box from "@mui/material/Box";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
+  { icon: <FileCopyIcon />, name: "Post", href: "/post" },
+  { icon: <SaveIcon />, name: "Save", href: "/post" },
+  { icon: <PrintIcon />, name: "Print", href: "/post" },
+  { icon: <ShareIcon />, name: "Share", href: "/post" },
 ];
 
 export default function ControlledOpenSpeedDial() {
-  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
   return (
-    <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+    <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
       <SpeedDial
         ariaLabel="SpeedDial controlled open example"
-        sx={{ position: 'absolute', bottom: 16, right: 16, }}
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
         icon={<SpeedDialIcon />}
         onClose={handleClose}
         onOpen={handleOpen}
@@ -35,7 +37,7 @@ export default function ControlledOpenSpeedDial() {
             key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
-            onClick={handleClose}
+            onClick={() => router.push(action.href)}
           />
         ))}
       </SpeedDial>
