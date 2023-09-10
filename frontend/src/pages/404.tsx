7ade0a91e976896e33d989c2c1c59a7b6b4e10b4
Ryohei <harrry.kuma1225@gmail.com>
2023-09-10 15:23:40 +0900

    add 404-page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image'
+
 export default function Custom404(): JSX.Element {
-  return <h1>ページが見つかりません</h1>
+  return (
+    <div className='not_page'>
+      <h1>404</h1>
+      <p>このページはすでに削除されているか、URLが間違っている可能性があります。</p>
+      <Image src='/404.jpg' width={600} height={500} alt='404の画像' className='not_page_img' />
+    </div>
+  )
 }
