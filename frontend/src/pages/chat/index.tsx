9013adb830532b3d16fcea130a16d5aeb925f2e6
Ryohei <harrry.kuma1225@gmail.com>
2023-09-13 02:45:46 +0900

    add no-user-func

--- frontend/src/pages/chat/index.tsx
+++ frontend/src/pages/chat/index.tsx
@@ -4,22 +4,23 @@ import Chat from 'components/template/chat'
 import apiClient from 'libs/apiClient'
 import { RootState } from 'store/store'
 import { RoomType } from 'types/global'
+import NoUser from 'components/widgets/NoUser'
 
 interface Props {
   rooms: RoomType[]
 }
 
-const ChatPage = ({ rooms }: Props) => {
-  const { userId, follow } = useSelector((state: RootState) => state.user)
+const ChatPage = ({ rooms }: Props): JSX.Element => {
+  const { userId } = useSelector((state: RootState) => state.user)
   useEffect(() => {
-    const roomFetch = async () => {
-     const res = await apiClient.get(`/chat/allrooms/${userId}`)
-     const rooms = res.data.rooms
+    const roomFetch = async (): Promise<void> => {
+      const res = await apiClient.get(`/chat/allrooms/${userId}`)
+      const rooms = res.data.rooms
     }
     roomFetch()
-  },[])
+  }, [userId])
 
-  return <Chat rooms={rooms} />
+  return <>{userId.length > 0 ? <Chat rooms={rooms} /> : <NoUser />}</>
 }
 
 export default ChatPage
--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux'
 import Mapage from 'components/template/mypage'
+import { RootState } from 'store/store'
+import NoUser from 'components/widgets/NoUser'
 
 const MypageFile = (): JSX.Element => {
-  return <Mapage />
+  const { userId } = useSelector((state: RootState) => state.user)
+  return <>{userId.length > 0 ? <Mapage /> : <NoUser />}</>
 }
 
 export default MypageFile
