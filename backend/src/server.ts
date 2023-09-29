ca808230b297dbdba639693466921e675ef6437f
Ryohei <harrry.kuma1225@gmail.com>
2023-09-29 17:51:26 +0900

    fix url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,17 +26,4 @@ app.use("/api/auth", authRoute);
 app.use("/api/board", boardRoute);
 app.use("/api/article", articleRoute);
 
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    // エラーハンドリングの処理
-    console.error(err);
-    res.status(500).send("Something broke!");
-  }
-);
-
 app.listen(port, () => console.log(`Listenings on port ${port}`));
