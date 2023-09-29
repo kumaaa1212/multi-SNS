4504c3cbb68ade23c35786efaa8d785b36ee7e9a
Ryohei <harrry.kuma1225@gmail.com>
2023-09-29 12:43:48 +0900

    fix url

--- frontend/src/libs/apiClient.ts
+++ frontend/src/libs/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  // baseURL: 'http://localhost:8000/api',
-  baseURL: process.env.NEXT_PUBLIC_AUTH_API_URL,
+  baseURL: 'https://backend-dgjc.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
