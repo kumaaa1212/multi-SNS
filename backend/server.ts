8b6f261ae527f4cb355bff9d3acf9049b99c527f
Ryohei <harrry.kuma1225@gmail.com>
2023-09-29 12:20:53 +0900

    fix server

--- backend/server.ts
+++ backend/server.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-const port = process.env.PORT  || 10000;
+const port = process.env.PORT || 10000;
 app.use(express.json());
 dotenv.config();
 
@@ -17,19 +17,17 @@ const corsOptions = {
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
   optionsSuccessStatus: 200,
-  Headers:{
-    "Access-Control-Allow-Origin": "https://tokotokoj-8vqdxztli-kumaaa1212.vercel.app",
-    "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  }
 };
 
 app.use(cors(corsOptions));
+
+// Allow preflight requests
+app.options('*', cors(corsOptions));
+
 app.use("/api/post", postRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/board", boardRoute);
 app.use("/api/article", articleRoute);
 
-app.listen(port, () => console.log(`Listenings on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
