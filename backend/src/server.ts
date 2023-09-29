911adda36dc38a831680147d375832fe934355f1
Ryohei <harrry.kuma1225@gmail.com>
2023-09-29 12:30:04 +0900

    fix file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import postRoute from "./src/router/post";
-import chatRoute from "./src/router/chat";
-import authRoute from "./src/router/auth";
-import boardRoute from "./src/router/board";
-import articleRoute from "./src/router/article";
+import postRoute from "./router/post";
+import chatRoute from "./router/chat";
+import authRoute from "./router/auth";
+import boardRoute from "./router/board";
+import articleRoute from "./router/article";
 import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT  || 10000;
 app.use(express.json());
 dotenv.config();
 
@@ -20,14 +20,10 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// Allow preflight requests
-app.options('*', cors(corsOptions));
-
 app.use("/api/post", postRoute);
 app.use("/api/chat", chatRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/board", boardRoute);
 app.use("/api/article", articleRoute);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listenings on port ${port}`));
