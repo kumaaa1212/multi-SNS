2c4d88af652a884eb3ea08a640fb01f0c29a7238
Ryohei <harrry.kuma1225@gmail.com>
2023-09-29 13:39:41 +0900

    fix api

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,27 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://tokotokoj-jyps54qck-kumaaa1212.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'OPTIONS, POST GET, PUT, DELETE',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
+    ]
+  },
   async redirects() {
     return [
       {
