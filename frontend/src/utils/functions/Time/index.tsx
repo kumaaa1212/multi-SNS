1bbb2d4a3182f40d76857bc32b2bacad4479698a
Ryohei <harrry.kuma1225@gmail.com>
2023-10-04 16:20:46 +0900

    fix err

--- frontend/src/utils/functions/Time/index.tsx
+++ frontend/src/utils/functions/Time/index.tsx
@@ -1,10 +1,13 @@
 export const formatTimestamp = (timestamp: string | undefined): string => {
   if (!timestamp) return ''
-  // const date = new Date(timestamp)
-  // const month = date.getMonth() + 1
-  // const day = date.getDate()
-  // const hours = date.getHours()
-  // const minutes = date.getMinutes()
 
-  return `${timestamp}`
+  const parts = timestamp.split(/[T:.Z-]/)
+
+  const year = parts[0]
+  const month = parseInt(parts[1], 10)
+  const day = parseInt(parts[2], 10)
+  const hours = parseInt(parts[3], 10)
+  const minutes = parseInt(parts[4], 10)
+
+  return `${year}/${month}/${day}/${hours}:${minutes}`
 }
