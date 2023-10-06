59f9be8c70d0c32e1968e82905a8c38f3060eb34
Ryohei <harrry.kuma1225@gmail.com>
2023-10-06 21:36:39 +0900

    fix stteper

--- frontend/src/components/parts/Button/Switch/index.tsx
+++ frontend/src/components/parts/Button/Switch/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Switch from '@mui/material/Switch'
-import stlye from './SwitchBtn.module.scss'
+import style from './SwitchBtn.module.scss'
 
 interface Props {
   keepPost: boolean
@@ -19,15 +19,17 @@ export default function SwitchBtn(props: Props): JSX.Element {
   }
 
   return (
-    <div className={`switch_btn ${!checked && stlye.keepoff}`}>
-      {relese ? (
-        <FormControlLabel control={<Switch checked={true} />} label='次へ進む' />
-      ) : (
-        <FormControlLabel
-          control={<Switch checked={checked} onChange={handleChange} />}
-          label='次へ進む'
-        />
-      )}
+    <div className='switch_btn_area'>
+      <div className={`switch_btn ${!checked && style.keepoff}`}>
+        {relese ? (
+          <FormControlLabel control={<Switch checked={true} />} label='次へ進む' />
+        ) : (
+          <FormControlLabel
+            control={<Switch checked={checked} onChange={handleChange} />}
+            label='次へ進む'
+          />
+        )}
+      </div>
     </div>
   )
 }
