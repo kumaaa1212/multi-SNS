3506b04e894aa7c5c24631fb856b3872ce4f43b4
Ryohei <harrry.kuma1225@gmail.com>
2023-10-18 01:28:51 +0900

    fix time

--- frontend/src/utils/functions/Time/index.tsx
+++ frontend/src/utils/functions/Time/index.tsx
@@ -1,13 +1,6 @@
 export const formatTimestamp = (timestamp: string | undefined): string => {
   if (!timestamp) return ''
 
-  const date = new Date(timestamp)
 
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-
-  return `${year}/${month}/${day}/${hours}:${minutes}`
+  return `${timestamp}`
 }
